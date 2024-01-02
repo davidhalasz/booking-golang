88daabffece3bbf557cad2db940c0be802479db1
davidhalasz <davidhalasz22@gmail.com>
2024-01-02 18:09:50 +0100

    added admin templates and display all reservations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,6 +12,7 @@ import (
 	"github.com/davidhalasz/go-bookings/internal/config"
 	"github.com/davidhalasz/go-bookings/internal/driver"
 	"github.com/davidhalasz/go-bookings/internal/forms"
+	"github.com/davidhalasz/go-bookings/internal/helpers"
 	"github.com/davidhalasz/go-bookings/internal/models"
 	"github.com/davidhalasz/go-bookings/internal/render"
 	"github.com/davidhalasz/go-bookings/internal/repository"
@@ -441,4 +442,27 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 
 func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+	reservations, err := m.DB.AllReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+
+	render.Template(w, r, "admin-all-reservations.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
+}
+
+func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
 }
--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -228,3 +228,68 @@ func (m *postgresDBRepo) Authenticate(email, testPassword string) (int, string, 
 
 	return id, hashedPassword, nil
 }
+
+func (m *postgresDBRepo) AllReservations() ([]models.Reservation, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var reservation []models.Reservation
+
+	query := `select r.id, r.first_name, r.last_name, r.email, r.phone, r.start_date, r.end_date, 
+		r.room_id, r.created_at, r.updated_at, 
+		rm.id, rm.room_name 
+		from reservations r 
+		left join rooms rm 
+		on(r.room_id=rm.id) 
+		order by r.start_date asc
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return reservation, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var i models.Reservation
+		var startDateStr string
+		var endDateStr string
+
+		err := rows.Scan(
+			&i.ID,
+			&i.FirstName,
+			&i.LastName,
+			&i.Email,
+			&i.Phone,
+			&startDateStr,
+			&endDateStr,
+			&i.RoomID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Room.ID,
+			&i.Room.RoomName,
+		)
+
+		if err != nil {
+			return reservation, err
+		}
+
+		i.StartDate, err = time.Parse("2006-01-02 15:04:05 -0700 MST", startDateStr)
+		if err != nil {
+			return reservation, err
+		}
+
+		i.EndDate, err = time.Parse("2006-01-02 15:04:05 -0700 MST", endDateStr)
+		if err != nil {
+			return reservation, err
+		}
+
+		reservation = append(reservation, i)
+	}
+
+	if err = rows.Err(); err != nil {
+		return reservation, err
+	}
+
+	return reservation, nil
+}
