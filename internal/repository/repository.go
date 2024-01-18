f884b5c8f9679b0199029f5673aa67f0f9f5909f
davidhalasz <davidhalasz22@gmail.com>
2024-01-18 18:44:46 +0100

    added methods get all rooms

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -12,6 +12,7 @@ type DatabaseRepo interface {
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
+	AllRooms() ([]models.Room, error)
 
 	GetUserByID(id int) (models.User, error)
 	UpdateUser(u models.User) error
